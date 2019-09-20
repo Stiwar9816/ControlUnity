a35adb77de4017414cad9f9b1afd7ef3cda45db1
JhonE17 <jhonepalacios17@gmail.com>
2019-09-19 21:35:05 -0500

    Routes

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,20 +3,12 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-<<<<<<< HEAD
+const _7a4de5be = () => interopDefault(import('../pages/home/index.vue' /* webpackChunkName: "pages/home/index" */))
 const _3b1a3adb = () => interopDefault(import('../pages/implementos/index.vue' /* webpackChunkName: "pages/implementos/index" */))
-const _a9b72f86 = () => interopDefault(import('../pages/login/index.vue' /* webpackChunkName: "pages/login/index" */))
 const _83fa3816 = () => interopDefault(import('../pages/registro/index.vue' /* webpackChunkName: "pages/registro/index" */))
 const _7941efbf = () => interopDefault(import('../pages/salones/index.vue' /* webpackChunkName: "pages/salones/index" */))
+const _17204847 = () => interopDefault(import('../pages/usuarios/index.vue' /* webpackChunkName: "pages/usuarios/index" */))
 const _c27adbba = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
-=======
-const _71e948f8 = () => interopDefault(import('../pages/home/index.vue' /* webpackChunkName: "pages/home/index" */))
-const _42688718 = () => interopDefault(import('../pages/implementos/index.vue' /* webpackChunkName: "pages/implementos/index" */))
-const _ac5d6e50 = () => interopDefault(import('../pages/registro/index.vue' /* webpackChunkName: "pages/registro/index" */))
-const _591c3b08 = () => interopDefault(import('../pages/salones/index.vue' /* webpackChunkName: "pages/salones/index" */))
-const _02eead2a = () => interopDefault(import('../pages/usuarios/index.vue' /* webpackChunkName: "pages/usuarios/index" */))
-const _2048dc40 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
->>>>>>> 9cd5157540eb50532b0a435b0376f5f3453b3021
 
 Vue.use(Router)
 
@@ -29,20 +21,13 @@ export const routerOptions = {
 
   routes: [{
       path: "/home",
-      component: _71e948f8,
+      component: _7a4de5be,
       name: "home"
     }, {
       path: "/implementos",
       component: _3b1a3adb,
       name: "implementos"
     }, {
-<<<<<<< HEAD
-      path: "/login",
-      component: _a9b72f86,
-      name: "login"
-    }, {
-=======
->>>>>>> 9cd5157540eb50532b0a435b0376f5f3453b3021
       path: "/registro",
       component: _83fa3816,
       name: "registro"
@@ -52,11 +37,11 @@ export const routerOptions = {
       name: "salones"
     }, {
       path: "/usuarios",
-      component: _02eead2a,
+      component: _17204847,
       name: "usuarios"
     }, {
       path: "/",
-      component: _2048dc40,
+      component: _c27adbba,
       name: "index"
     }],
 
