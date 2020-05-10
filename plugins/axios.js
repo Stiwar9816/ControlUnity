d368bc0839df4c92fd81a5bff9e37d8bb2162709
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-05-09 22:01:01 -0500

    a

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
  import * as axios from '@nuxtjs/axios'
 
+ 
 let options = {}
 if (process.server) {
   options.baseURL = `http://${process.env.MONGODB_URI || 'localhost'}:${process.env.PORT || 3000}`
