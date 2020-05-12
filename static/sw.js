2ab973e7638f9554c9b6ea362dab248e198ee9f1
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-05-12 02:26:47 -0500

    sw configured

--- static/sw.js
+++ static/sw.js
@@ -19,6 +19,42 @@ workbox.precaching.cleanupOutdatedCaches()
 
 // --------------------------------------------------
 // Precaches
+  // Cache Google Fonts with a stale-while-revalidate strategy, with
+  // a maximum number of entries.
+  registerRoute(
+    ({url}) => url.origin === 'https://fonts.googleapis.com' ||
+               url.origin === 'https://fonts.gstatic.com',
+    new StaleWhileRevalidate({
+      cacheName: 'google-fonts',
+      plugins: [
+        new ExpirationPlugin({maxEntries: 20}),
+      ],
+    }),
+  );
+
+  //Css And JS
+  registerRoute(
+    ({request}) => request.destination === 'script' ||
+                   request.destination === 'style',
+    new StaleWhileRevalidate()
+  );
+
+  //Images
+  registerRoute(
+    ({request}) => request.destination === 'image',
+    new CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
 // --------------------------------------------------
 
 // Precache assets
@@ -30,3 +66,4 @@ workbox.precaching.cleanupOutdatedCaches()
 // Register route handlers for runtimeCaching
 workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('https://control-unity-api.herokuapp.com'), new workbox.strategies.NetworkFirst ({}), 'GET')
